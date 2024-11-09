dfe07310a89dfaf5bf5b86632e39c82947e919f6
Вадим Голов <grafit-design@inbox.ru>
2024-11-09 13:45:44 +0500

    делаю gui

--- dynamic_gui.py
+++ dynamic_gui.py
@@ -42,44 +42,52 @@ def apply_style():
 # Создание окна
 root: tk = tk.Tk()
 root.title('Создание Python virtual environment GUI')
-root.tk.call('tk', 'scaling', 1.0)
+# root.tk.call('tk', 'scaling', 1.0)
 
-# Ширина и высота окна
-win_width = 640
-win_height = 480
+# Размещение окна в центре экрана
+win_width = 800
+win_height = 335
 
 scr_width = root.winfo_screenwidth()
 scr_height = root.winfo_screenheight()
 
-# Вычисляем координаты для размещения окна в центре
 x_pos = (scr_width // 2) - (win_width // 2)
 y_pos = (scr_height // 2) - (win_height // 2)
 
-# Устанавливаем размер и положение окна
 root.geometry(f'{win_width}x{win_height}+{x_pos}+{y_pos}')
 
-apply_style()
-
-# for style_name, style_properties in styles.items():
-#     style.configure(style_name, **style_properties)
-
+# Иконка окна
 icon = PhotoImage(file='images/main_icon.png')
 root.iconphoto(False, icon)
 
-frame: ttk = ttk.Frame(root)
-frame.pack(fill=tk.BOTH, expand=True)
+# Фоновое изображение
+background = PhotoImage(file='images/background.png')
+
+canvas = tk.Canvas(root, width=800, height=335)
+canvas.pack(fill="both", expand=True)
+
+# Установите изображение в холст
+canvas.create_image(0, 0, image=background, anchor="nw")
+
+
+
+
+
+
+# frame.create_image(0, 0, image=bkg, anchor='nw')
+
 
 # Добавление виджетов
-path_label: ttk = ttk.Label(frame, text='Задайте размещение нового виртуального окружения', anchor='center')
-path_label.place(x=120, y=5, width=400, height=22)
-
-task_font: tuple[str, int] = ('Arial', 11)  # через стили в settings={} не работало не смог понять почему
-
-task_entry = ttk.Entry(frame, font=task_font)
-task_entry.place(x=5, y=30, width=600, height=25)
-
-add_button: ttk = ttk.Button(frame, text='...')
-add_button.place(x=610, y=30, width=25, height=25)
+# path_label: ttk = ttk.Label(frame, text='Задайте размещение нового виртуального окружения', anchor='center')
+# path_label.place(x=120, y=5, width=400, height=22)
+#
+# task_font: tuple[str, int] = ('Arial', 11)  # через стили в settings={} не работало не смог понять почему
+#
+# task_entry = ttk.Entry(frame, font=task_font)
+# task_entry.place(x=5, y=30, width=600, height=25)
+#
+# add_button: ttk = ttk.Button(frame, text='...')
+# add_button.place(x=610, y=30, width=25, height=25)
 
 
 
